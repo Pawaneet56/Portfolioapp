43f5645516bf513c00e664fda9291f57e7e48bdb
Pawaneet56 <pawaneetsingh56@gmail.com>
2021-01-03 21:58:16 +0530

    improved logout button ..

--- app/src/main/java/com/example/portfolioapp/HomeActivity.java
+++ app/src/main/java/com/example/portfolioapp/HomeActivity.java
@@ -68,11 +68,10 @@ private FirebaseAuth f;
                 builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-<<<<<<< HEAD
+
                         f.signOut();
-=======
+
                         FirebaseAuth.getInstance().signOut();
->>>>>>> 9971639200d89e2463707b9e54ba8e23a86ca36d
                         Toast.makeText(HomeActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(HomeActivity.this,Startactivity.class));
                         finish();
