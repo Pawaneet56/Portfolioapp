379dc893b836ca3d530be4c7d8f5b95de6b2ee07
Sanidhya2101 <Sanidhya2101@users.noreply.github.com>
2021-01-03 18:38:33 +0530

    I updated the navbar

--- app/src/main/java/com/example/portfolioapp/HomeActivity.java
+++ app/src/main/java/com/example/portfolioapp/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.portfolioapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -17,10 +18,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 private DrawerLayout drawerLayout;
 
     @Override
@@ -28,75 +29,69 @@ private DrawerLayout drawerLayout;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        drawerLayout=findViewById(R.id.drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    }
+        drawerLayout = findViewById(R.id.drawer);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open,R.string.close);
 
-    public void ClickMenu(View view){
-        openDrawer(drawerLayout);
-    }
-    static void openDrawer(DrawerLayout drawerLayout) {
-        drawerLayout.openDrawer(GravityCompat.START);
-    }
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-
-    public void ClickLogo(View view){
-        closeDrawer(drawerLayout);
-    }
-    static void closeDrawer(DrawerLayout drawerLayout) {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-    }
-
-
-    public void ClickHome(View view) {
-        Toast.makeText(HomeActivity.this, "Home",Toast.LENGTH_SHORT).show();
-        recreate();
-    }
-
-
-
-    public void ClickProfile(View view){
-        Toast.makeText(HomeActivity.this, "Your Profile",Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(HomeActivity.this,ProfileActivity.class));
-        finish();
-    }
-
-
-
-    public  void ClickLogout(View view){
-        logout(this);
-    }
-   private void logout(Activity activity) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(activity);
-        builder.setTitle("LOGOUT");
-        builder.setMessage("Are you sure you want to logout ?");
-
-        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(HomeActivity.this, "Logged out Succesfully", Toast.LENGTH_SHORT).show();
-                activity.startActivity(new Intent( HomeActivity.this,Startactivity.class));
-                finish();
-            }
-        });
-
-        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        builder.show();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        closeDrawer(drawerLayout);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.home:
+                Toast.makeText(this, "HOME", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                break;
+
+            case R.id.profile:
+                Toast.makeText(this, "YOUR PROFILE", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                        new ProfileFragment()).commit();
+                break;
+
+            case R.id.logout:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("LOGOUT");
+                builder.setMessage("Are you sure you want to logout ?");
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(HomeActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), Startactivity.class));
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
 
