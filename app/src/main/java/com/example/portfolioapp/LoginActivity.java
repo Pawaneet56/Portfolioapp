ef55ea71eb9252dc7ddcc4b1a47a36658dda71ea
Pawaneet56 <pawaneetsingh56@gmail.com>
2021-01-06 01:48:04 +0530

    multidex

--- app/src/main/java/com/example/portfolioapp/LoginActivity.java
+++ app/src/main/java/com/example/portfolioapp/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
