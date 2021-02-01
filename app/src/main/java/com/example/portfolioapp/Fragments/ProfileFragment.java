74c0e787f1ed383d6ab1d58216fbca6f8dfbda9f
Pawaneet56 <Pawaneetsingh56@gmail.com>
2021-02-01 22:24:30 +0530

    please correct it

--- app/src/main/java/com/example/portfolioapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/portfolioapp/Fragments/ProfileFragment.java
@@ -1,7 +1,14 @@
 package com.example.portfolioapp.Fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
@@ -21,6 +29,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
+import java.io.File;
 import java.text.BreakIterator;
 import java.util.ArrayList;
 
@@ -42,6 +51,9 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 import org.jetbrains.annotations.NotNull;
@@ -51,6 +63,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.grpc.Compressor;
+
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
 public class ProfileFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
     private View MultiSpinner;
@@ -59,16 +75,29 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
     private FirebaseFirestore fstore;
     private EditText fname;
     private EditText femail;
+    private Bitmap compressor;
     private NumberPicker fyear;
     private EditText fbio;
     private Button button;
+    private Context mcontext;
     private Button button2;
     private ProgressBar pg;
-
+    private ProgressDialog progressDialog;
+    ImageView myimage;
+    private Uri ImageUri;
+    String downloadurl;
+    private StorageReference storageReference;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
 
     @Nullable
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mcontext = context;
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Profile Fragment");
         // Inflate the layout for this fragment
@@ -86,25 +115,48 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
         fname = view.findViewById(R.id.et_name_cp);
         button = view.findViewById(R.id.btn);
         femail = view.findViewById(R.id.et_Email_cp);
-        fyear=view.findViewById(R.id.et_year_cp);
+        fyear = view.findViewById(R.id.et_year_cp);
         fyear.setMaxValue(2020);
         fyear.setMinValue(1970);
-        fbio=view.findViewById(R.id.et_bio_cp);
+        fbio = view.findViewById(R.id.et_bio_cp);
         button2 = view.findViewById(R.id.btn_cp);
         pg = view.findViewById(R.id.progressbar_cp);
+        myimage = view.findViewById(R.id.iv_cp);
         fstore = FirebaseFirestore.getInstance();
         f = FirebaseAuth.getInstance();
+        storageReference= FirebaseStorage.getInstance().getReference("users");
+        myimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(mcontext, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        pickImageFromGallery();
+
+                    }
+
+                } else {
+                    pickImageFromGallery();
+
+                }
+            }
+        });
         fstore.collection("users").document(f.getCurrentUser().getUid().toString()).get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         if (documentSnapshot.exists()) {
-                            fname.setText(documentSnapshot.getString("Full Name"));
+                            fname.setText(documentSnapshot.getString("FullName"));
                             femail.setText(documentSnapshot.getString("Email"));
+
 
                             fyear.setValue(documentSnapshot.getLong("Year").intValue());
                             fbio.setText(documentSnapshot.getString("Bio"));
-                            int po=documentSnapshot.getLong("college").intValue();
+
+
+                            int po = documentSnapshot.getLong("college").intValue();
                             spinner.setSelection(po);
 
                             fname.setEnabled(false);
@@ -113,6 +165,7 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
                             fbio.setEnabled(false);
                             spinner.setEnabled(false);
                             button2.setEnabled(false);
+                            myimage.setEnabled(false);
                         }
                     }
                 });
@@ -124,6 +177,7 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
                 fyear.setEnabled(true);
                 fbio.setEnabled(true);
                 spinner.setEnabled(true);
+                myimage.setEnabled(true);
 
             }
         });
@@ -134,6 +188,8 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
                 String email = femail.getText().toString();
                 int year = fyear.getValue();
                 String bio = fbio.getText().toString();
+                String image=myimage.toString();
+                String downloadurl1=downloadurl.toString();
 
                 UpdateProfile(pname, bio, year);
                 fname.setEnabled(false);
@@ -142,8 +198,10 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
                 spinner.setEnabled(false);
                 fbio.setEnabled(false);
                 SavingPost(pname);
+                myimage.setEnabled(false);
 
                 pg.setVisibility(View.GONE);
+
             }
         });
 
@@ -151,15 +209,16 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemSelec
 
     }
 
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (position == 0) {
             Toast.makeText(getContext(), "Please Select your College", Toast.LENGTH_SHORT).show();
 
         } else {
-            String current_id=f.getCurrentUser().getUid();
-int hold=spinner.getSelectedItemPosition();
-fstore.collection("users").document(current_id).update("college",hold);
+            String current_id = f.getCurrentUser().getUid();
+            int hold = spinner.getSelectedItemPosition();
+            fstore.collection("users").document(current_id).update("college", hold);
 
 
         }
@@ -169,20 +228,40 @@ fstore.collection("users").document(current_id).update("college",hold);
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-    private void UpdateProfile(String name,String bio,int year) {
-        String current_id=f.getCurrentUser().getUid();
-        fstore.collection("users").document(current_id).update("Full Name",name);
-        fstore.collection("users").document(current_id).update("Bio",bio);
 
-        fstore.collection("users").document(current_id).update("Year",year);
-        Toast.makeText(getActivity(),"Updated",Toast.LENGTH_SHORT).show();
+    private void UpdateProfile(String name, String bio, int year) {
+        String current_id = f.getCurrentUser().getUid();
+        fstore.collection("users").document(current_id).update("Full Name", name);
+        fstore.collection("users").document(current_id).update("Bio", bio);
+
+        fstore.collection("users").document(current_id).update("Year", year);
+        StorageReference imagereference=storageReference.child(ImageUri.getLastPathSegment());
+        imagereference.putFile(ImageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while(!uriTask.isSuccessful());
+
+                        downloadurl = uriTask.getResult().toString();
+                        if(uriTask.isSuccessful())
+                        {
+String id=f.getCurrentUser().getUid().toString();
+fstore.collection("users").document(id).update("Image",downloadurl);
+                        }
+                    }});
+
+
+
+
+        Toast.makeText(getActivity(), "Updated", Toast.LENGTH_SHORT).show();
     }
 
 
     private void SavingPost(String name) {
         String current_id = f.getCurrentUser().getUid().toString();
-        CollectionReference dp=fstore.collection("Posts");
-        dp.whereEqualTo("Id",current_id).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        CollectionReference dp = fstore.collection("Posts");
+        dp.whereEqualTo("Id", current_id).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
@@ -192,11 +271,38 @@ fstore.collection("users").document(current_id).update("college",hold);
                         dp.document(document.getId()).set(doc, SetOptions.merge());
                     }
 
-            }
-                else{
-                    Toast.makeText(getActivity(),"sorry",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "sorry", Toast.LENGTH_SHORT).show();
                 }
-        }});
+            }
+        });
     }
-}
 
+    private void pickImageFromGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK);
+        gallery.setType("image/*");
+        startActivityForResult(gallery, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(mcontext, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE && data != null) {
+            ImageUri = data.getData();
+            myimage.setImageURI(ImageUri);
+
+        }
+    }
+
+}
